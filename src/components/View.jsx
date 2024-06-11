52a4c576fb4d6c0f9a5ff61ae8fbe938d974b20e
Gloriya2001 <glooriaa2001@gmail.com>
2024-06-11 06:16:35 +0530

    view done

---
+++ src/components/View.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+
+const View = () => {
+    return (
+        <div>
+
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <h1><b>Add Product Details</b></h1>
+                        <div className="row g-3">
+                            
+                                <h1>Student Profile</h1>
+
+                                <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                                    <div className="card">
+                                        <img src="#" class="card-img-top" alt="...">
+                                        </img>
+                                        <div className="card-body">
+                                            <h5 className="card-title">aaa</h5>
+                                            <h3>aaa</h3>
+                                            <a href="#" class="btn btn-info">Add to cart</a>
+                                        </div>
+                                    </div>
+
+
+                       
+
+
+
+
+
+
+
+                            </div>
+
+
+
+                        </div>
+                    </div>
+                </div>
+                </div>
+
+
+            </div>
+            )
+}
+
+            export default View
