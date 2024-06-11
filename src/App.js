ab1f069079caf60df14ffa34fcc4bd38dbefb27f
Gloriya2001 <glooriaa2001@gmail.com>
2024-06-11 06:20:53 +0530

    Navebar done

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import Add from './components/Add';
 import View from './components/View';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div >
+      <Navbar/>
       <Add/>
       <View/>
     </div>
